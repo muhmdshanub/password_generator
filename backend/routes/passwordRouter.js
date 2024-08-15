e85c97073d0230052e863648001b725ec5d38efc
muhmdshanub <muhmdshanoob@gmail.com>
2024-08-15 23:34:28 +0530

    password generator api implemented

--- backend/routes/passwordRouter.js
+++ backend/routes/passwordRouter.js
@@ -3,8 +3,10 @@
 const express = require('express');
 const router = express.Router();
 const { getPassword } = require('../controllers/passwordController');
+const validate = require('../middlewares/validationMiddleware.js');
+const {validatePasswordParams} = require('../validators/passwordValidator.js')
 
 // Define the route for getting a password
-router.get('/generate', getPassword);
+router.get('/generate', validatePasswordParams, validate, getPassword);
 
 module.exports = router;
